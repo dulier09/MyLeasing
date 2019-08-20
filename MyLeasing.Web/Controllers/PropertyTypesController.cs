9e43a30220a9e697698ba783358ec7671ca2263b
DULIER ARMANDO MU�OZ ZAPATA <duliermunoz163703@correo.itm.edu.co>
2019-08-19 19:19:26 -0500

    Add Property

--- MyLeasing.Web/Controllers/PropertyTypesController.cs
+++ MyLeasing.Web/Controllers/PropertyTypesController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyLeasing.Web.Data;
 using MyLeasing.Web.Data.Entities;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyLeasing.Web.Controllers
 {
