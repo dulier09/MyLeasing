dc36b2f34f6643af2c3951ae681a0f310bc26973
DULIER ARMANDO MU�OZ ZAPATA <duliermunoz163703@correo.itm.edu.co>
2019-08-23 19:12:37 -0500

    Delete Image-Contract

--- MyLeasing.Web/Helpers/IConverterHelper.cs
+++ MyLeasing.Web/Helpers/IConverterHelper.cs
@@ -7,7 +7,11 @@ namespace MyLeasing.Web.Helpers
     public interface IConverterHelper
     {
         Task<Property> ToPropertyAsync(PropertyViewModel model, bool isNew);
+
         PropertyViewModel ToPropertyViewModel(Property property);
+
         Task<Contract> ToContractAsync(ContractViewModel model, bool isNew);
+
+        ContractViewModel ToContractViewModel(Contract contract);
     }
 }
